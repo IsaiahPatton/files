c0ff55e10a1d7226d197928a4fcc8ef2e7dce442
Isaiah <IsaiahPatton@users.noreply.github.com>
2019-05-29 16:09:08 -0400

    Update note.js

--- note.js
+++ note.js
@@ -1,10 +1,8 @@
-
 function showNote() {
     document.getElementById("zunozapnote").innerHTML = "<div class=\"zznote\"><div class=\"zzhead\">Switch to ZunoZap</div>" +
             "<span onclick=\"this.parentElement.style.display=\'none\';\">x</span><div class=\"zznote-container\">"
                 + "<p>This site recommends using <a href=\"http://zunozap.com/\">ZunoZap</a>, a fast and new browser. Try it?</p>"
-                + "<a href='http://zunozap.com/'><button>YES</button></a>" +
-            "</div></div>";
+                + "<a href='http://zunozap.com/'><button>YES</button></a></div></div>";
 }
 
-if (!navigator.userAgent.toString().includes("ZunoZap")) showNote();
+if (!navigator.userAgent.toString().includes("ZunoZap") || window.location.href.split("?").toString().includes("no_zunozap_note")) showNote();
